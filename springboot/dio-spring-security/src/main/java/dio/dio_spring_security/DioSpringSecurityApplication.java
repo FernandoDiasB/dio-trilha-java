f33e5ba0c48576a007d26364576f0541bd641dbf
Fernando Dias <fernando.sdbarros@gmail.com>
2025-05-05 19:34:17 -0300

    commit

--- springboot/dio-spring-security/src/main/java/dio/dio_spring_security/DioSpringSecurityApplication.java
+++ springboot/dio-spring-security/src/main/java/dio/dio_spring_security/DioSpringSecurityApplication.java
@@ -1,14 +1,13 @@
-package dio.projeto_web_api;
+package dio.dio_spring_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoWebApiApplication {
+public class DioSpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoWebApiApplication.class, args);
+		SpringApplication.run(DioSpringSecurityApplication.class, args);
 	}
 
 }
- 
